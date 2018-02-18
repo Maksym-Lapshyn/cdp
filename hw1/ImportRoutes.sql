06f82f218270d5a9801f1b8480de0d72de702ac6
maxon-adc <lapshyn.maksym@gmail.com>
2018-02-18 18:14:39 +0200

    Added ImportRoutes script

---
+++ hw1/ImportRoutes.sql
@@ -0,0 +1,15 @@
+USE XYZ;
+
+INSERT INTO dbo.Route(
+	OriginId,
+	DestinationId,
+	Distance
+)
+SELECT DISTINCT
+    origin.Id,
+    destination.Id,
+	FLOOR(RAND(CHECKSUM(NEWID()))*(3000 - 100 +1) + 100)
+FROM 
+    dbo.Warehouse AS origin,
+    dbo.Warehouse AS destination
+WHERE origin.Id != destination.Id;
