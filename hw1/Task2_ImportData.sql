2cd1eb3e7d7cc504e1b84e721497254b439519e6
maxon-adc <lapshyn.maksym@gmail.com>
2018-02-24 14:24:32 +0200

    Created stored procedures for selecting drivers

--- hw1/Task2_ImportData.sql
+++ hw1/Task2_ImportData.sql
@@ -2,18 +2,18 @@ BEGIN TRANSACTION;
 
 USE Shipment_ML;
 
-EXEC dbo.ImportCSV 'dbo.Truck', 'D:\Projects\CDP\hw1\ImportData\Trucks.csv';
+EXEC dbo.ImportCSV 'dbo.Truck', 'E:\Projects\CDP\hw1\ImportData\Trucks.csv';
 
 GO
 
-EXEC dbo.ImportCSV 'dbo.Driver', 'D:\Projects\CDP\hw1\ImportData\Drivers.csv';
+EXEC dbo.ImportCSV 'dbo.Driver', 'E:\Projects\CDP\hw1\ImportData\Drivers.csv';
 
 GO
 
-EXEC dbo.ImportCSV 'dbo.Warehouse', 'D:\Projects\CDP\hw1\ImportData\Warehouses.csv';
+EXEC dbo.ImportCSV 'dbo.Warehouse', 'E:\Projects\CDP\hw1\ImportData\Warehouses.csv';
 
 GO
 
-EXEC dbo.ImportCSV 'dbo.DriverTruck', 'D:\Projects\CDP\hw1\ImportData\Drivers-Trucks.csv';
+EXEC dbo.ImportCSV 'dbo.DriverTruck', 'E:\Projects\CDP\hw1\ImportData\Drivers-Trucks.csv';
 
 COMMIT TRANSACTION;
---
+++ hw2/Task1_CreateProcedures.sql
@@ -0,0 +1,38 @@
+USE Shipment_ML;
+
+GO
+
+/*Procedure using EXEC*/
+
+CREATE PROCEDURE dbo.GetDriversExec (@fieldName NVARCHAR(100), @fieldValue NVARCHAR(100))
+AS BEGIN
+	DECLARE @sqlCommand NVARCHAR(500);
+
+	SET @sqlCommand = 
+		'SELECT Firstname, Lastname, Birthdate FROM dbo.Driver WHERE ' + @fieldName +
+		' = ''' + @fieldValue  + ''';';
+
+	EXEC(@sqlCommand);
+END
+
+GO
+
+/*Procedure using EXECUTE sp_executesql*/
+
+CREATE PROCEDURE dbo.GetDriversExecuteSQL (@fieldName NVARCHAR(100), @fieldValue NVARCHAR(100))
+AS BEGIN
+	DECLARE @sqlCommand NVARCHAR(500);
+
+	SET @sqlCommand = 'SELECT Firstname, Lastname, Birthdate FROM dbo.Driver WHERE ' + @fieldName + '= @fieldValue;';
+
+	EXECUTE sp_executesql @sqlCommand, N'@fieldName NVARCHAR(100), @fieldValue NVARCHAR(100)',  @fieldName, @fieldValue
+END
+
+GO
+
+/*Test for procedures*/
+
+DECLARE @fieldName NVARCHAR(100) = 'FirstName';
+DECLARE @fieldValue NVARCHAR(100) = 'John';
+
+EXEC dbo.GetDriversExecuteSQL @fieldName, @fieldValue;
