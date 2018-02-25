62bc3704f7a0ab5a06ca92e15047a014f8118694
maxon-adc <lapshyn.maksym@gmail.com>
2018-02-25 14:23:29 +0200

    Finished queries that delete duplicate truck

---
+++ hw2/Task2_DeleteQueries.sql
@@ -0,0 +1,18 @@
+USE Shipment_ML;
+
+/*FIRST QUERY*/
+
+DELETE x FROM (
+	SELECT *, rn = ROW_NUMBER() OVER (PARTITION BY RegistrationNumber ORDER BY ID ASC)
+	FROM dbo.Truck 
+) x
+WHERE rn > 1;
+
+/*SECOND QUERY*/
+
+DELETE x FROM (
+	SELECT TOP 1 *, total = COUNT(*) OVER (PARTITION BY RegistrationNumber)
+	FROM dbo.Truck
+	ORDER BY Id DESC
+) x
+WHERE total > 1;
