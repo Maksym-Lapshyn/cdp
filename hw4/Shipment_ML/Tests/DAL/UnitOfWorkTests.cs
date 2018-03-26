eb99d588dc4484a23eff8b4b6a72c256a8db13c2
Maksym Lapshyn <Maksym_Lapshyn@epam.com>
2018-03-26 18:28:47 +0300

    Created test project.

---
+++ hw4/Shipment_ML/Tests/DAL/UnitOfWorkTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Xunit;
+
+namespace Tests.DAL
+{
+	public class UnitOfWorkTests : IDisposable
+	{
+		private readonly SqlConnection _connection;
+		private readonly SqlCommand _getWarehouseCountCommand;
+		private readonly SqlCommand _getRouteCountCommand;
+
+		public UnitOfWorkTests()
+		{
+			_connection = new SqlConnection("Data Source=.;Integrated Security=True;Initial Catalog=Shipment_ML;");
+			_getWarehouseCountCommand = GetSqlCommand("[dbo].[Warehouse]");
+			_getRouteCountCommand = GetSqlCommand("[dbo].[Route]");
+			
+			_connection.Open();
+
+			var warehouseCount = _getWarehouseCountCommand.ExecuteScalar();
+			var routeCount = _getRouteCountCommand.ExecuteScalar();
+
+			_connection.Close();
+		}
+
+		public void Dispose()
+		{
+			_connection.Open();
+
+			var warehouseCount = _getWarehouseCountCommand.ExecuteScalar();
+			var routeCount = _getRouteCountCommand.ExecuteScalar();
+
+			_connection.Close();
+			_connection.Dispose();
+		}
+
+		[Fact]
+		public void TestSomething()
+		{
+
+		}
+
+		private SqlCommand GetSqlCommand(string tableName)
+		{
+			var command = new SqlCommand("GetRowCount", _connection)
+			{
+				CommandType = CommandType.StoredProcedure,
+
+				Parameters =
+				{
+					new SqlParameter("@tableName", SqlDbType.NVarChar)
+					{
+						Value = tableName
+					}
+				}
+			};
+
+			return command;
+		}
+	}
+}
---
+++ hw4/Shipment_ML/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("512902c8-eb0b-4936-8a2d-e1ca719a428a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
