4ec116dffbd528bc0efaac95002f1041d345a179
Maksym Lapshyn <Maksym_Lapshyn@epam.com>
2018-03-27 10:22:31 +0300

    Finished integration tests.

--- hw4/Shipment_ML/Tests/DAL/UnitOfWorkTests.cs
+++ hw4/Shipment_ML/Tests/DAL/UnitOfWorkTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.Entities;
+using DAL.UnitOfWork.Implementation;
+using System;
 using System.Data;
 using System.Data.SqlClient;
 using Xunit;
@@ -7,15 +9,19 @@ namespace Tests.DAL
 {
 	public class UnitOfWorkTests : IDisposable
 	{
+		private const string ConnectionString = "Data Source=.;Integrated Security=True;Initial Catalog=Shipment_ML;";
+		private const string WarehouseTableName = "[dbo].[Warehouse]";
+		private const string RouteTableName = "[dbo].[Route]";
+
 		private readonly SqlConnection _connection;
 		private readonly SqlCommand _getWarehouseCountCommand;
 		private readonly SqlCommand _getRouteCountCommand;
 
 		public UnitOfWorkTests()
 		{
-			_connection = new SqlConnection("Data Source=.;Integrated Security=True;Initial Catalog=Shipment_ML;");
-			_getWarehouseCountCommand = GetSqlCommand("[dbo].[Warehouse]");
-			_getRouteCountCommand = GetSqlCommand("[dbo].[Route]");
+			_connection = new SqlConnection(ConnectionString);
+			_getWarehouseCountCommand = GetSqlCommand(WarehouseTableName);
+			_getRouteCountCommand = GetSqlCommand(RouteTableName);
 			
 			_connection.Open();
 
@@ -37,9 +43,44 @@ namespace Tests.DAL
 		}
 
 		[Fact]
-		public void TestSomething()
+		public void Uow_CreatesAndReadsRoutesAndWarehouses()
 		{
+			var uow = new UnitOfWork();
 
+			var origin = new Warehouse
+			{
+				City = "Gotham City",
+				State = "New Jersey"
+			};
+
+			var destination = new Warehouse
+			{
+				City = "Metropolis",
+				State = "New Jersey"
+			};
+
+			uow.BeginTransaction(IsolationLevel.ReadUncommitted);
+
+			origin = uow.WarehouseRepository.Create(origin);
+			destination = uow.WarehouseRepository.Create(destination);
+
+			var route = new Route
+			{
+				OriginId = origin.Id,
+				DestinationId = destination.Id,
+				Distance = 1000
+			};
+
+			route = uow.RouteRepository.Create(route);
+
+			var routeFromDatabase = uow.RouteRepository.ReadOne(route.Id);
+
+			Assert.Equal(route.Id, routeFromDatabase.Id);
+			Assert.Equal(origin.Id, routeFromDatabase.OriginId);
+			Assert.Equal(route.Distance, routeFromDatabase.Distance);
+			Assert.Equal(destination.Id, routeFromDatabase.DestinationId);
+
+			uow.RollbackTransaction();
 		}
 
 		private SqlCommand GetSqlCommand(string tableName)
